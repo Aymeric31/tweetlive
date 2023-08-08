a6db06168603bf3727851a6267bfa0a4ee25e037
Aymeric31 <aymeric.gioia@hotmail.fr>
2023-08-08 10:53:38 +0200

    discord embed notification added, rename function to get_twitch_info, new information get (started_at, thumbnail, title), the started_at as been converted into france timezone with the format wanted

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 import json
 import smtplib
 import re
+import pytz
 
 load_dotenv()
 
@@ -19,6 +20,8 @@ twitch_username = os.environ.get('TWITCH_USERNAME')
 twitch_client_id = os.environ.get('TWITCH_CLIENT_ID')
 twitch_access_token = os.environ.get('TWITCH_ACCESS_TOKEN')
 twitch_client_secret = os.environ.get('TWITCH_CLIENT_SECRET')
+
+discord_webhook = os.environ.get('DISCORD_WEBHOOK')
 
 def get_remaining_time(twitch_client_id, twitch_client_secret):
     # Endpoint de validation du token
@@ -95,7 +98,18 @@ def send_email():
         # Fermeture de la connexion au serveur SMTP
         server.quit()
 
-def is_user_live(twitch_username, twitch_client_id, twitch_access_token, twitch_client_secret):
+def format_started_at(started_at):
+    # Convertir la date et l'heure de début au format ISO 8601 en objet datetime
+    started_at_datetime = datetime.fromisoformat(started_at.replace('Z', '+00:00'))
+    
+    # Convertir en heure française (Europe/Paris)
+    timezone_france = pytz.timezone('Europe/Paris')
+    started_at_france = started_at_datetime.astimezone(timezone_france)
+
+    formatted_started_at = started_at_france.strftime("Aujourd'hui à %Hh%M")
+    return formatted_started_at
+
+def get_twitch_live_info(twitch_username, twitch_client_id, twitch_access_token, twitch_client_secret):
     url = f"https://api.twitch.tv/helix/streams?user_login={twitch_username}"
     headers = {
         "Client-ID": twitch_client_id,
@@ -106,14 +120,59 @@ def is_user_live(twitch_username, twitch_client_id, twitch_access_token, twitch_
     data = response.json()
     get_remaining_time(twitch_client_id, twitch_client_secret)
 
-
     if "data" in data and len(data["data"]) > 0:
-        game = data["data"][0]["game_name"].replace(" ", "")
-        game = re.sub(r'[^\w\s]', '', game)
-        return True, game  # L'utilisateur est en direct sur tel jeu
+        stream_data = data["data"][0]
+        game_name = stream_data["game_name"].replace(" ", "")
+        game_name = re.sub(r'[^\w\s]', '', game_name)
+        started_at = stream_data["started_at"]  # Date et heure de début du stream au format ISO 8601
+        formatted_started_at = format_started_at(started_at)  # Formatter la date et l'heure de début
+        thumbnail_url = stream_data["thumbnail_url"].replace("{width}", "1280").replace("{height}", "720")
+        title = stream_data["title"]
+        return True, game_name, formatted_started_at, thumbnail_url, title  # L'utilisateur est en direct
     else:
-        return False, None  # L'utilisateur n'est pas en direct
-    
+        return False, None, None, None  # L'utilisateur n'est pas en direct
+
+# Fonction pour envoyer le message Discord au format embed
+def send_discord_embed(discord_webhook, twitch_username, twitch_live_info):
+    if twitch_live_info:
+        game_name = twitch_live_info["game_name"]
+        formatted_started_at = twitch_live_info["formatted_started_at"]
+        thumbnail_url = twitch_live_info["thumbnail_url"]
+        title = twitch_live_info["title"]
+        content = f"@everyone {twitch_username} est en live sur Twitch <:Twitch:707494410778050620>!"
+
+        embed_data = {
+            "title": title,
+            "url": f"https://www.twitch.tv/{twitch_username}",
+            "color": 9520895,  # Couleur du message (vous pouvez changer cette valeur)
+            "image": {
+                "url": thumbnail_url,
+            },
+            "author": {
+                "name": f"{twitch_username} est en direct 🍕"
+            },
+            "fields": [
+                {
+                    "name": ":joystick: Jeu", 
+                    "value": game_name,
+                    "inline": True
+                },
+                {
+                    "name": ":red_circle: Début du stream",
+                    "value": formatted_started_at,
+                    "inline": True
+                }
+            ]
+        }
+
+        data = {"content": content, "embeds": [embed_data]}
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(f"https://discord.com/api/webhooks/{discord_webhook}", data=json.dumps(data), headers=headers)
+        if response.status_code == 204:
+            print("Message Discord envoyé avec succès !")
+        else:
+            print(f"Échec lors de l'envoi du message Discord : {response.status_code}")
+
 def send_tweet(twitter_consumer_key, twitter_consumer_secret, twitter_access_token, twitter_access_token_secret, twitter_bearer_token, tweet_text):
     client = tweepy.Client( 
         twitter_bearer_token, 
@@ -131,14 +190,21 @@ def send_tweet(twitter_consumer_key, twitter_consumer_secret, twitter_access_tok
         file.write(tweet_id)
 
 def check_user_live(twitch_username, twitch_client_id, twitter_consumer_key, twitter_consumer_secret, twitter_access_token, twitter_access_token_secret, twitter_bearer_token):
-    is_live, game = is_user_live(twitch_username, twitch_client_id, twitch_access_token, twitch_client_secret)
+    is_live, game_name, formatted_started_at, thumbnail_url, title = get_twitch_live_info(twitch_username, twitch_client_id, twitch_access_token, twitch_client_secret)
 
     if is_live:
-        tweet_text = f"Je suis en direct sur Twitch sur #{game} rejoins moi ! ⬇️ https://www.twitch.tv/{twitch_username}"
+        tweet_text = f"Je suis en direct sur Twitch sur #{game_name} rejoins moi ! ⬇️ https://www.twitch.tv/{twitch_username}"
         send_tweet(twitter_consumer_key, twitter_consumer_secret, twitter_access_token, twitter_access_token_secret, twitter_bearer_token, tweet_text)
-        print(f"L'utilisateur {twitch_username} est en direct sur Twitch à 21h10.")
+        # Appel de la fonction pour envoyer l'embed Discord
+        send_discord_embed(discord_webhook, twitch_username, {
+            "title" : title,
+            "game_name": game_name,
+            "formatted_started_at": formatted_started_at,
+            "thumbnail_url": thumbnail_url
+        })
+        print(f"L'utilisateur {twitch_username} est en direct sur Twitch à {formatted_started_at}.")
         print(f"Tweet envoyé : {tweet_text}")
     else:
-        print(f"L'utilisateur {twitch_username} n'est pas en direct sur Twitch à 21h10.")
+        print(f"L'utilisateur {twitch_username} n'est pas en direct sur Twitch.")
 
 check_user_live(twitch_username, twitch_client_id, twitter_consumer_key, twitter_consumer_secret, twitter_access_token, twitter_access_token_secret, twitter_bearer_token)
