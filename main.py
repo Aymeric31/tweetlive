481d8b94a9bad9cd033c64f08e3335d071009491
Aymeric Gioia <aymeric.gioia@hotmail.fr>
2025-04-14 15:40:49 +0200

    Improve error handling in send_discord_message and send_tweet functions
    - Centralized error handling directly within the send_discord_message and send_tweet functions.
    - Removed redundant try/except blocks from higher-level functions like check_user_live.
    - Updated all comments in the code to English for better clarity and consistency.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import tweepy
 import requests
 import os
-from dotenv import load_dotenv
-from datetime import datetime, timedelta
 import json
 import smtplib
 import re
 import pytz
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -24,7 +24,7 @@ twitch_client_secret = os.environ.get('TWITCH_CLIENT_SECRET')
 discord_webhook = os.environ.get('DISCORD_WEBHOOK')
 
 def get_remaining_time(twitch_client_id, twitch_client_secret):
-    # Endpoint de validation du token
+    # Token validation endpoint
     endpoint = "https://id.twitch.tv/oauth2/token"
     headers = {
         "Content-Type": "application/x-www-form-urlencoded"
@@ -35,59 +35,61 @@ def get_remaining_time(twitch_client_id, twitch_client_secret):
         "grant_type": "client_credentials"
     }
     response = requests.post(endpoint, headers=headers, data=data)
-    print(response)
+    
     if response.status_code == 200:
-        # Conversion de la réponse en JSON
+        # Convert the response to JSON
         data = json.loads(response.text)
 
-        # Récupération du timestamp d'expiration du token
+        # Retrieve the token expiration timestamp
         expires_at = data["expires_in"]
 
-        # Conversion du timestamp en objet de date et heure
+        # Convert the expiration timestamp to a datetime object
         expires_at_datetime = datetime.now() + timedelta(seconds=expires_at)
 
-        # Calcul de la durée restante avant l'expiration
+        # Calculate the remaining duration before expiration
         remaining_time = expires_at_datetime - datetime.now()
 
         print(f"Temps restant avant expiration : {remaining_time}")
         
-        # Vérification si la durée restante est de 7 jours
+        # Check if the remaining duration is 7 days or less
         if remaining_time.days <= 7:
             send_email()
-    elif response.status_code == 401:
-        print("Le token Twitch n'est pas valide (401 Unauthorized).")
-    else:
-        print("Échec de la validation du token")
 
-# Fonction pour envoyer un e-mail
+    elif response.status_code == 401:
+        print("The Twitch token is not valid (401 Unauthorized).")
+
+    else:
+        print("Token validation failed")
+
+# Function to send an email
 def send_email():
-    # Configuration du serveur SMTP et des informations d'authentification
+    # SMTP server configuration and authentication information
     smtp_server = 'smtp.gmail.com'
     smtp_port = 587
     smtp_username = os.environ.get('SMTP_USERNAME')
     smtp_password = os.environ.get('SMTP_PASSWORD')
 
-    # Informations de l'expéditeur et du destinataire
+    # Sender and recipient information
     sender_email = os.environ.get('SENDER_EMAIL')
     recipient_email = os.environ.get('RECIPIENT_EMAIL')
 
-    # Ajouter le lien vers le référentiel GitHub
+    # Add link to GitHub secrets of the repository
     repo_name = os.environ.get('REPO_NAME')
     github_secrets_url = f"https://github.com/{repo_name}/settings/secrets"
 
-    # Création du message
+    # Message creation
     subject = '[APP Tweetlive] Token expiration notification'
     body = f"Your token will expire in less than 7 days. Please take necessary actions here: {github_secrets_url}."
 
     message = f'Subject: {subject}\n\n{body}'
 
     try:
-        # Connexion au serveur SMTP
+        # SMTP server connexion 
         server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls()
         server.login(smtp_username, smtp_password)
 
-        # Envoi de l'e-mail
+        # Send the email
         server.sendmail(sender_email, recipient_email, message)
         print('E-mail sent successfully')
 
@@ -95,14 +97,14 @@ def send_email():
         print(f'Error sending e-mail: {str(e)}')
 
     finally:
-        # Fermeture de la connexion au serveur SMTP
+        # Close the connection to the SMTP server
         server.quit()
 
 def format_started_at(started_at):
-    # Convertir la date et l'heure de début au format ISO 8601 en objet datetime
+    # Convert the start date and time from ISO 8601 format to a datetime object
     started_at_datetime = datetime.fromisoformat(started_at.replace('Z', '+00:00'))
     
-    # Convertir en heure française (Europe/Paris)
+    # Convert to French time (Europe/Paris)
     timezone_france = pytz.timezone('Europe/Paris')
     started_at_france = started_at_datetime.astimezone(timezone_france)
 
@@ -122,8 +124,8 @@ def format_stream_data(stream_data):
     game_name_exact = stream_data["game_name"]
     game_name_tweet = game_name_exact.replace(" ", "")
     game_name_tweet = re.sub(r'[^\w\s]', '', game_name_tweet)
-    started_at = stream_data["started_at"]  # Date et heure de début du stream au format ISO 8601
-    formatted_started_at = format_started_at(started_at)  # Formatter la date et l'heure de début
+    started_at = stream_data["started_at"]  # Date and time of the start of the stream in ISO 8601 format
+    formatted_started_at = format_started_at(started_at)  # Format date and time of the start of the stream
     thumbnail_url = stream_data["thumbnail_url"].replace("{width}", "1280").replace("{height}", "720")
     title = stream_data["title"]
     return game_name_exact, game_name_tweet, formatted_started_at, thumbnail_url, title      
@@ -138,12 +140,12 @@ def get_twitch_live_info(twitch_username, twitch_client_id, twitch_access_token,
     response = requests.get(url, headers=headers)
     data = response.json()
     get_remaining_time(twitch_client_id, twitch_client_secret)
-    print(data)
+    
     if "data" in data and len(data["data"]) > 0:
         stream_data = data["data"][0]
         print(stream_data)
         game_name_exact, game_name_tweet, formatted_started_at, thumbnail_url, title = format_stream_data(stream_data)
-        return StreamInfo(True, game_name_exact, game_name_tweet, formatted_started_at, thumbnail_url, title)  # L'utilisateur est en direct
+        return StreamInfo(True, game_name_exact, game_name_tweet, formatted_started_at, thumbnail_url, title)  # User is live
     else:
         return StreamInfo(False)
 
@@ -156,7 +158,7 @@ def construct_embed_data(twitch_username, twitch_live_info):
     embed_data = {
         "title": title,
         "url": f"https://www.twitch.tv/{twitch_username}",
-        "color": 9520895,  # Couleur du message (vous pouvez changer cette valeur)
+        "color": 9520895,  # Color of the message (you can change this value)
         "image": {
             "url": thumbnail_url,
         },
@@ -182,14 +184,17 @@ def construct_embed_data(twitch_username, twitch_live_info):
 def send_discord_message(discord_webhook, twitch_username, twitch_live_info):
     if twitch_live_info:
         embed_data = construct_embed_data(twitch_username, twitch_live_info)
-        content = f"@everyone {twitch_username} est en live sur Twitch <:Twitch:707494410778050620>!"
+        # Set content for the message, emoji ID for the Twitch emoji
+        content = f"everyone {twitch_username} est en live sur Twitch <:Twitch:707494410778050620>!"
         data = {"content": content, "embeds": [embed_data]}
         headers = {"Content-Type": "application/json"}
-        response = requests.post(f"https://discord.com/api/webhooks/{discord_webhook}", data=json.dumps(data), headers=headers)
-        if response.status_code == 204:
-            print("Message Discord envoyé avec succès !")
-        else:
-            print(f"Échec lors de l'envoi du message Discord : {response.status_code}")
+        
+        try:
+            response = requests.post(f"https://discord.com/api/webhooks/{discord_webhook}", data=json.dumps(data), headers=headers)
+            response.raise_for_status()  # Raise an exception for HTTP errors
+            print("Discord message sent successfully!")
+        except requests.exceptions.RequestException as e:
+            print(f"Error while sending the Discord message: {e}")
 
 
 def send_tweet(twitter_consumer_key, twitter_consumer_secret, twitter_access_token, twitter_access_token_secret, twitter_bearer_token, tweet_text):
@@ -201,24 +206,41 @@ def send_tweet(twitter_consumer_key, twitter_consumer_secret, twitter_access_tok
         twitter_access_token_secret, 
         wait_on_rate_limit=True
         )
-    # Envoi du tweet
-    response = client.create_tweet(text=tweet_text)
-    tweet_id = str(response.data['id'])
+    
+    try:
+        # Send the tweet
+        response = client.create_tweet(text=tweet_text)
+        tweet_id = str(response.data['id'])
 
-    with open("tweet-id.txt", "w") as file:
-        file.write(tweet_id)
+        # Save the tweet ID to a file
+        with open("tweet-id.txt", "w") as file:
+            file.write(tweet_id)
+
+        print(f"Tweet sent successfully! Tweet ID: {tweet_id}")
+    except tweepy.TweepyException as e:
+        print(f"Error while sending the tweet: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred while sending the tweet: {e}")
 
 def check_user_live(twitch_username, twitch_client_id, twitter_consumer_key, twitter_consumer_secret, twitter_access_token, twitter_access_token_secret, twitter_bearer_token):
-    twitch_live_info = get_twitch_live_info(twitch_username, twitch_client_id, twitch_access_token, twitch_client_secret)
+    try:
+        # Check if the user is live on Twitch
+        twitch_live_info = get_twitch_live_info(twitch_username, twitch_client_id, twitch_access_token, twitch_client_secret)
 
-    if twitch_live_info.is_live:
-        tweet_text = f"Je suis en direct sur Twitch sur #{twitch_live_info.game_name_tweet} rejoins moi ! ⬇️ https://www.twitch.tv/{twitch_username}"
-        send_tweet(twitter_consumer_key, twitter_consumer_secret, twitter_access_token, twitter_access_token_secret, twitter_bearer_token, tweet_text)
-        # Appel de la fonction pour envoyer l'embed Discord
-        send_discord_message(discord_webhook, twitch_username, twitch_live_info)
-        print(f"L'utilisateur {twitch_username} est en direct sur Twitch à {twitch_live_info.formatted_started_at}.")
-        print(f"Tweet envoyé : {tweet_text}")
-    else:
-        print(f"L'utilisateur {twitch_username} n'est pas en direct sur Twitch.")
+        if twitch_live_info.is_live:
+
+            tweet_text = f"Je suis en direct sur Twitch sur #{twitch_live_info.game_name_tweet} rejoins moi ! ⬇️ https://www.twitch.tv/{twitch_username}"
+            
+            # Send the tweet
+            send_tweet(twitter_consumer_key, twitter_consumer_secret, twitter_access_token, twitter_access_token_secret, twitter_bearer_token, tweet_text)
+
+            # Send the message on Discord
+            send_discord_message(discord_webhook, twitch_username, twitch_live_info)
+
+            print(f"The user {twitch_username} has been live on Twitch since {twitch_live_info.formatted_started_at}.")
+        else:
+            print(f"The user {twitch_username} is not live on Twitch.")
+    except Exception as e:
+        print(f"Error while checking Twitch status: {e}")
 
 check_user_live(twitch_username, twitch_client_id, twitter_consumer_key, twitter_consumer_secret, twitter_access_token, twitter_access_token_secret, twitter_bearer_token)
